ea6aaa4637192cefdde5a6362e43a256ec53941d
ChrisCode3 <malusdarkblade66@gmail.com>
2023-03-01 07:29:02 +0000

    added basic questions to inquirer prompt

--- starter/index.js
+++ starter/index.js
@@ -3,9 +3,67 @@ const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
+// array of questions for user to ansswer to create the basic stracture of the readme
 const questions = [
+    inquirer.prompt ([
+        {
+        type: "input",
+        name:"title",
+        message: "What is the title called ?"
+    },
+    
+    {
+        type: "input",
+        name:"author",
+        message: "What is your name ?"
+    },
+    
+    {
+        type: "input",
+        name:"github",
+        message: "What is your github name?"
+    },
 
+    {
+        type: "input",
+        name:"installation",
+        message: "Any requirements for the installation",
+    },
+
+    {
+        type: "input",
+        name:"description",
+        message: "A brief description of the project please",
+    },
+
+
+    {
+        type: "input",
+        name:"usage",
+        message: "What is the this applications intended use? To put it simply what is this designed to to?",
+    },
+
+
+    {
+        type: "input",
+        name:"contributing",
+        message: "Was there any significant contribution to this project ?",
+    },
+
+
+    {
+        type: "list",
+        name:"license",
+        message: "what licence did you use?",
+        choices: ["MIT","Apache license 2.0","Creative Commons Attribution 4.0", "The GPL Licence", "GNU License",]	
+
+    }
+
+    ]
+
+
+
+    )
 ];
 
 // function to write README file
